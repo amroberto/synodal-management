9e101fd20676de8d349ea0343aad140d78ce2ac6
Alexandre M. Roberto <amroberto@gmail.com>
2026-07-24 16:16:46 -0300

    Add OfferInstanceEnum enum for offer types

---
+++ app/Enums/OfferInstance.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Enums;
+
+enum OfferInstance: string
+{
+    case LOCAL = 'local';
+    case SINODAL = 'sinodal';
+    case NATIONAL = 'national';
+    case ESPECIAL = 'especial';
+
+    /**
+     * Nome amigável.
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::LOCAL => 'Local',
+            self::SINODAL => 'Sinodal',
+            self::NATIONAL => 'Nacional',
+            self::ESPECIAL => 'Especial';
+        };
+    }
+
+    /**
+     * Opções para Select.
+     */
+    public static function options(): array
+    {
+        return collect(self::cases())
+            ->mapWithKeys(fn (self $item) => [
+                $item->value => $item->label(),
+            ])
+            ->toArray();
+    }
+
+    /**
+     * Cor para badges.
+     */
+    public function color(): string
+    {
+        return match ($this) {
+            self::LOCAL => 'zinc',
+            self::SINODAL => 'blue',
+            self::NATIONAL => 'green',
+            self::ESPECIAL => 'cyan',
+        };
+    }
+}
